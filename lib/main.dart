e502250daffa862011afbacd1895222390db8455
RaphTech-Solutions <famouszegenee@gmail.com>
2022-04-22 03:08:09 +0100

    padding class is added to the first card widget

--- lib/main.dart
+++ lib/main.dart
@@ -43,32 +43,40 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              // using a card widget instead of a container widget
+              Card(
+                // padding: EdgeInsets.all(10.0), This is not recognised in card
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.pink[900],
-                    ),
-                    SizedBox(
-                      width: 30.0,
-                    ),
-                    Text(
-                      '+23470 3161 2572',
-                      style: TextStyle(
+
+                // This is how a padding class is included into a card class
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
                         color: Colors.pink[900],
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 15.0,
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        width: 30.0,
+                      ),
+                      Text(
+                        '+23470 3161 2572',
+                        style: TextStyle(
+                          color: Colors.pink[900],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 15.0,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+
+              // using a card widget instead of a container widget
+              Card(
+                //padding: EdgeInsets.all(10.0),
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: Row(
