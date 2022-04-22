e307ffbbeac776018ed1b1c44aa0f1986d475163
RaphTech-Solutions <famouszegenee@gmail.com>
2022-04-22 02:14:11 +0100

    my contact card app

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.pink[300],
+        appBar: AppBar(
+          title: Text('Layout demo'),
+          backgroundColor: Colors.pink[900],
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/raph.jpg'),
+              ),
+              Text(
+                'Zegenee Raphael',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELLOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 15.0,
+                  color: Colors.pink[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.pink[900],
+                    ),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    Text(
+                      '+23470 3161 2572',
+                      style: TextStyle(
+                        color: Colors.pink[900],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.pink[900],
+                    ),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    Text(
+                      'famouszegenee@gmail.com',
+                      style: TextStyle(
+                        color: Colors.pink[900],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
